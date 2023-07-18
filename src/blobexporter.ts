e821f64782e9e9d072c07954c85533166378898a
gamedev44 <108845184+gamedev44@users.noreply.github.com>
2023-07-18 17:23:05 -0500

    Updated blobexporter.ts

--- src/blobexporter.ts
+++ src/blobexporter.ts
@@ -1,11 +1,20 @@
 import axios from 'axios';
 import fs from 'fs';
+import { lz4 } from 'lz4';
 
 // Assume you have an access token for authentication
 const accessToken = 'YOUR_ACCESS_TOKEN';
 
 // Define the URL of the Dreams API server
 const apiUrl = 'https://api.dreams.example.com';
+
+// Function to decompress LZ4 data
+function decompressLZ4(data: Buffer): Buffer {
+  // Decompress the LZ4 data using the lz4 library or module
+  const decompressedData = lz4.decompress(data);
+
+  return decompressedData;
+}
 
 // Function to export blob data from the server
 async function exportBlobData() {
@@ -18,11 +27,22 @@ async function exportBlobData() {
       responseType: 'arraybuffer',
     });
 
-    // Save the blob data to a file
-    const blobData = Buffer.from(response.data, 'binary');
-    fs.writeFileSync('exported_blob.bin', blobData);
+    // Handle LZ4 compression if multiple blob files are present
+    if (response.headers['content-encoding'] === 'lz4') {
+      // Decompress the LZ4 data stream
+      const decompressedData = decompressLZ4(Buffer.from(response.data));
 
-    console.log('Blob data exported successfully!');
+      // Save the decompressed blob data to a file
+      fs.writeFileSync('decompressed_blob.bin', decompressedData);
+
+      console.log('LZ4 data decompressed successfully!');
+    } else {
+      // Save the blob data to a file
+      const blobData = Buffer.from(response.data, 'binary');
+      fs.writeFileSync('exported_blob.bin', blobData);
+
+      console.log('Blob data exported successfully!');
+    }
   } catch (error) {
     console.error('Failed to export blob data:', error.message);
   }
